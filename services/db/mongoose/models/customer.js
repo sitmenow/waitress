4d51fe8b868325f71385a93da4e2e0e56833d013
Gerardo Reyes <grevych@gmail.com>
2018-12-11 11:55:02 -0600

    Add reference to restaurant to customer mongoose model

--- services/db/mongoose/models/customer.js
+++ services/db/mongoose/models/customer.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
   name: String,
-  restaurantid: mongoose.ObjectId,
+  restaurantId: mongoose.ObjectId,
 });
 
 module.exports = mongoose.model('Customer', schema);
