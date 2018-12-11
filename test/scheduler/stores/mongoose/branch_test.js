e0072f4c2a1e28884a3805e936e19f55e4a3a3e7
Gerardo Reyes <grevych@gmail.com>
2018-12-11 12:06:58 -0600

    Add test scenario 'model not created' to branch mongoose store test

--- test/scheduler/stores/mongoose/branch_test.js
+++ test/scheduler/stores/mongoose/branch_test.js
@@ -1,4 +1,5 @@
 const { assert, expect } = require('chai');
+const sinon = require('sinon');
 const mongoose = require('mongoose');
 
 const Branch = require('../../../../scheduler/branch');
@@ -16,23 +17,25 @@ suite('Mongoose BranchStore', () => {
   suite('#find()', () => {
 
     suiteSetup(() => {
+      sandbox = sinon.createSandbox();
       branchModel = new BranchModel({
         name: 'test',
-      });
-      expectedBranch = new Branch({
-        id: branchModel.id,
-        name: branchModel.name,
       });
       return branchModel.save();
     });
 
     suiteTeardown(() => {
+      sandbox.restore();
       return branchModel.delete();
     });
 
     test('returns a branch with the given id', async () => {
       // BranchModel.findById(branchModel._id)
       //   .then(_ => console.log(_));
+      const expectedBranch = new Branch({
+        id: branchModel.id,
+        name: branchModel.name,
+      });
       const branch = await branchStore.find(branchModel.id);
 
       assert.deepEqual(expectedBranch, branch);
@@ -47,6 +50,17 @@ suite('Mongoose BranchStore', () => {
           //       will be considered as non-completed. Be careful with
           //       expected types
           expect(error).to.be.instanceof(branchStoreErrors.BranchNotFound);
+          done();
+        });
+    });
+
+    test('throws a branch not created error', (done) => {
+      sandbox.stub(branchStore, '_modelToObject')
+        .throws(new branchStoreErrors.BranchNotCreated());
+
+      branchStore.find(branchModel.id)
+        .catch((error) => {
+          expect(error).to.be.instanceof(branchStoreErrors.BranchNotCreated);
           done();
         });
     });
