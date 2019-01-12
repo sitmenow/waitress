c5a90944d23a51a312adb80f8d9afc16163605dd
Gerardo Reyes <grevych@gmail.com>
2019-01-11 23:38:41 -0600

    Add method 'find' to mongoose hostess store

--- scheduler/stores/mongoose/hostess.js
+++ scheduler/stores/mongoose/hostess.js
@@ -1,14 +1,38 @@
-class Hostess {
-  constructor(connection) {
-    this.connection = connection;
+const Hostess = require('../../hostess');
+const HostessModel = require('../../../services/db/mongoose/models/hostess');
+const storeErrors = require('../errors');
+
+class HostessStore {
+
+  async create(hostess) {
   }
 
-  create(hostess) {
-  }
+  async find(hostessId) {
+    const model = await HostessModel.findById(hostessId);
 
-  find(hostessId) {
+    if (!model) throw new storeErrors.HostessNotFound(hostessId);
+
+    return this._modelToObject(model);
   }
 
   update(hostess) {
   }
+
+  _modelToObject(model) {
+    let hostess = null;
+
+    try {
+      hostess = new Hostess({
+        id: model.id,
+        name: model.name,
+      });
+    } catch (error) {
+      console.log(error)
+      throw new storeErrors.HostessNotCreated();
+    }
+
+    return hostess;
+  }
 }
+
+module.exports = HostessStore;
