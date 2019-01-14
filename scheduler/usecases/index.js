6576078a7c0ea399444db0c7e43c569ce71af33f
Gerardo Reyes <grevych@gmail.com>
2019-01-14 00:10:02 -0600

    Expose use cases through use cases index file

--- scheduler/usecases/index.js
+++ scheduler/usecases/index.js
@@ -1,9 +1,15 @@
-const adminUseCases = require('./admin');
-const customerUseCases = require('./customer');
-const hostessUseCases = require('./hostess');
+// const adminUseCases = require('./admin');
+const CustomerCreateTurn = require('./customer/create-turn');
+const CustomerDetailTurn = require('./customer/detail-turn');
 
-module.exports = (stores, services) => {
-  admin: adminUseCases(stores, services),
-  customer: customerUseCases(stores, services),
-  hostess: hostessUseCases(stores, services),
+const HostessListGasTurns = require('./hostess/list-gas-turns');
+const HostessServeGasTurn = require('./hostess/serve-gas-turn');
+const HostessRejectGasTurn = require('./hostess/reject-gas-turn');
+
+module.exports = {
+  CustomerCreateTurn,
+  CustomerDetailTurn,
+  HostessListGasTurns,
+  HostessServeGasTurn,
+  HostessRejectGasTurn,
 };
