11d6071636a803be021c02c4330cdab3e2c79ab9
Gerardo Reyes <grevych@gmail.com>
2019-01-14 22:33:16 -0600

    Add functions to mongoose store test helper

--- test/scheduler/stores/mongoose/store_test_helper.js
+++ test/scheduler/stores/mongoose/store_test_helper.js
@@ -2,30 +2,68 @@ const config = require('config');
 
 const database = require('../../../../scheduler/stores/mongoose');
 require('../../test_helper')
+const RestaurantModel = require('../../../../services/db/mongoose/models/restaurant');
 const BranchModel = require('../../../../services/db/mongoose/models/branch');
 const HostessModel = require('../../../../services/db/mongoose/models/hostess');
+const TurnModel = require('../../../../services/db/mongoose/models/turn');
+// const RestaurantStore = require('../../../../scheduler/stores/mongoose/restaurant');
 const HostessStore = require('../../../../scheduler/stores/mongoose/hostess');
+const BranchStore = require('../../../../scheduler/stores/mongoose/branch');
+const CustomerStore = require('../../../../scheduler/stores/mongoose/customer');
+const TurnStore = require('../../../../scheduler/stores/mongoose/turn');
 
 
 before(() => {
   mongoose = database(config)
     .catch(error => console.log(`Error while connecting to database: ${error}`));
 
-  createBranchModel = ({ branchName, restaurantId }) => {
-    return new BranchModel({
+  createRestaurantModel = ({ restaurantId, restaurantName }) => {
+    return new RestaurantModel({
       id: restaurantId,
-      name: branchName,
+      name: restaurantName,
     });
   };
 
-  createHostessModel = ({ hostessName, branchId }) => {
-    return new HostessModel({
+  createBranchModel = ({
+    branchId,
+    branchName,
+    branchAddress,
+    lastOpeningTime,
+    lastClosingTime,
+    coordinates,
+    restaurantId,
+  }) => {
+    return new BranchModel({
       id: branchId,
-      name: hostessName,
+      name: branchName,
+      address: branchAddress,
+      location: {
+        type: 'Point',
+        coordinates,
+      },
+      lastOpeningTime,
+      lastClosingTime,
+      restaurantId,
     });
   };
+
+  createHostessModel = ({ hostessName, hostessId, branchId }) => {
+    return new HostessModel({
+      id: hostessId,
+      name: hostessName,
+      branchId,
+    });
+  };
+
+  createTurnModel = ({}) => {};
 
   createHostessStore = () => new HostessStore();
+
+  createBranchStore = () => new BranchStore();
+
+  createCustomerStore = () => new CustomerStore();
+
+  createTurnStore = () => new TurnStore();
 
   return mongoose;
 });
