b20bd599f274687307182f49c9919fede8fc347f
Gerardo Reyes <grevych@gmail.com>
2019-01-14 21:04:38 -0600

    Update mongoose model 'branch':  - Add property 'lastOpeningTime'  - Add property 'lastClosingTime'  - Add property location (point)  - Add property 'address'

--- services/db/mongoose/models/branch.js
+++ services/db/mongoose/models/branch.js
@@ -3,6 +3,20 @@ const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
   name: String,
+  address: String,
+  lastOpeningTime: Date,
+  lastClosingTime: Date,
+  location: {
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true,
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+    },
+  },
   restaurantId: mongoose.ObjectId,
 });
 
