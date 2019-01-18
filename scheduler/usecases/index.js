82366c938ca3d503b61a897a816a56ad64a6dada
Gerardo Reyes <grevych@gmail.com>
2019-01-17 18:17:44 -0600

    Expose all gas station use cases in use cases index file

--- scheduler/usecases/index.js
+++ scheduler/usecases/index.js
@@ -1,19 +1,27 @@
 // const adminUseCases = require('./admin');
 const CustomerCreateTurn = require('./customer/create-turn');
 const CustomerDetailTurn = require('./customer/detail-turn');
+
+
 const CustomerListGasTurns = require('./customer/list-gas-turns');
 const CustomerCreateGasTurn = require('./customer/create-gas-turn');
+const CustomerDetailGasTurn = require('./customer/detail-gas-turn');
 
 const HostessListGasTurns = require('./hostess/list-gas-turns');
 const HostessServeGasTurn = require('./hostess/serve-gas-turn');
 const HostessRejectGasTurn = require('./hostess/reject-gas-turn');
+const HostessToggleGasStation = require('./hostess/toggle-gas-station');
 
 module.exports = {
   CustomerCreateTurn,
   CustomerDetailTurn,
+
   CustomerListGasTurns,
+  CustomerDetailGasTurn,
   CustomerCreateGasTurn,
+
   HostessListGasTurns,
+  HostessToggleGasStation,
   HostessServeGasTurn,
   HostessRejectGasTurn,
 };
