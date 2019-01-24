baae259749e7a26cb1ed992e37f7fe8eb23e79d3
Gerardo Reyes <grevych@gmail.com>
2019-01-24 00:56:39 -0600

    Add script to seed branch and hostess in local envs

--- config/develop.js
+++ config/develop.js
@@ -2,6 +2,9 @@ module.exports = {
   api: {
     port: 8080,
   },
+  entities: {
+    hostess: '5c4955c210728692df56a6a4',
+  },
   services: {
     db: {
       database: "sit_me_now_dev",
---
+++ seed.js
@@ -0,0 +1,50 @@
+const config = require('config');
+const minimist = require('minimist');
+const mongoose = require('mongoose');
+
+const mongooseStore = require('./scheduler/stores/mongoose');
+const HostessModel = require('./services/db/mongoose/models/hostess');
+const BranchModel = require('./services/db/mongoose/models/branch');
+
+
+const argv = minimist(process.argv.slice(2));
+console.log(argv);
+
+async function createBranch() {
+  const model = new BranchModel({
+    name: 'Branch Seed',
+    location: {
+      type: 'Point',
+      coordinates: [10, 10],
+    },
+    isOpen: true,
+    address: 'Branch Seed Address',
+    restaurantId: mongoose.Types.ObjectId(),
+  });
+
+  await model.save();
+  console.log(model);
+
+  return model;
+}
+
+async function createHostess(branch) {
+  const model = new HostessModel({
+    _id: mongoose.Types.ObjectId(config.entities.hostess),
+    name: 'Hostess Seed',
+    branchId: branch.id,
+  });
+
+  await model.save();
+  console.log(model);
+
+  return model;
+}
+
+
+mongooseStore(config)
+  .then(createBranch)
+  .then(createHostess)
+  .catch(error =>
+    console.log(`Error while connecting to Mongo: ${error}`)
+  );
