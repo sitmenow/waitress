86a4911c72628900b1958125d43a063bc69691cf
Gerardo Reyes <grevych@gmail.com>
2019-05-11 17:13:56 -0500

    Update config default module:  - Add property structure to manage slack secret  - Add property structure to manage slack tokens

--- config/develop.js
+++ config/develop.js
@@ -3,9 +3,6 @@ module.exports = {
     origin: '*',
     port: 8080,
   },
-  entities: {
-    hostess: '5c4955c210728692df56a6a4',
-  },
   services: {
     db: {
       database: "sit_me_now_dev",
